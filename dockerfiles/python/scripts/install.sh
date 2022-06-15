6ac9604bcac134f84a553fc7b0402b3f6714832d
Ваше Имя <you@example.com>
2022-06-15 14:36:15 +0500

    up

--- dockerfiles/python/scripts/install.sh
+++ dockerfiles/python/scripts/install.sh
@@ -9,27 +9,27 @@ apk add libressl-dev
 apk add libffi-dev
 
 pip3 install debugpy
-pip3 install beautifulsoup4
-pip3 install requests
+# pip3 install beautifulsoup4
+# pip3 install requests
 
-apk add libxml2-dev
-apk add libxslt-dev
+# apk add libxml2-dev
+# apk add libxslt-dev
 
-pip3 install lxml
+# pip3 install lxml
 
-apk add jpeg-dev
+# apk add jpeg-dev
 
-pip3 install Pillow
+# pip3 install Pillow
 
-pip3 install PyMySQL
+# pip3 install PyMySQL
 
 pip3 install --upgrade pip
 # pip3 install ptvsd
 # pip3 install websocket-client
 # pip3 install PyMySQL
-pip3 install gtts
+pip3 install gtts==2.2.4
 # pip3 install inbox.py
-pip3 install pyftpdlib
-pip3 install pyopenssl
+pip3 install pyftpdlib==1.5.4
+pip3 install pyopenssl==22.0.0
 
 apk add supervisor
